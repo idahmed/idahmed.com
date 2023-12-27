3aa6a0f08f8b579fe2ff7837f642d3aff0afd873
idahmed yassine <idahmedyassine@gmail.com>
2023-12-27 15:46:37 +0100

    update status

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Hi, my name is Yassine, I am a python developer building server side
+          Hi, my name is Yassine, I am a senior python developer building server side
           applications using latest and most popular web-technologies.
         </h2>
       </div>
